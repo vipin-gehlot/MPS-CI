3b99a9077f7a3cf507ca4b90f2892988f4997da4
Vipin <vipin.gehlot@ishisystems.com>
2016-02-22 15:09:07 +0530

    Gradle file changed.

--- src/test/groovy/com/yt/mps/jenkins/test/JobScriptsSpec.groovy
+++ src/test/groovy/com/yt/mps/jenkins/test/JobScriptsSpec.groovy
@@ -33,7 +33,7 @@ class JobScriptsSpec extends Specification {
     static List<File> getJobFiles() {
         List<File> files = []
         new File('jobs').eachFileRecurse(FileType.FILES) {
-            if (it.name.endsWith('.groovy')) {
+            if (it.name.endsWith('mps*.groovy')) {
                 files << it
             }
         }
